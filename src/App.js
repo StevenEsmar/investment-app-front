ace0ae8d4d28c3524381c6056c833de26993971b
StevenEsmar <stespinosama@unal.edu.co>
2020-07-13 19:51:49 -0500

    add briefcase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import iconNotify from '../src/assets/notify.png';
 import iconCircle from '../src/assets/circle.png';
+import donut from '../src/assets/s_donut.png';
+import donut2 from '../src/assets/s_donut2.png';
 
 
 function App() {
@@ -51,12 +53,27 @@ function App() {
               <div className="imgIsland"/>
             </div>
             <p className="textGoal1">Viaje a islas Canarias</p>
+            <div className="lineVector"></div>
+            <div className="frameData">
+                <p className="dataGoals">$1.630.000</p>
+                <p className="dataGoals2">$8.000.000</p>
+                <img src={donut} className="donutIcon"></img>
+
+            </div>
         </div>  
         <div className="goals2">
             <div className="framePc">
               <div className="imgPc"/>
             </div>
             <p className="textGoal2">Macbook Pro</p>
+            <div className="lineVector"></div>
+            <div className="frameData">
+                <p className="dataGoals">$0</p>
+                <p className="dataGoals2">$7.000.000</p>
+                <img src={donut2} className="donutIcon"></img>
+                
+
+            </div>
         </div> 
       
     </div>
